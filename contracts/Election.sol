a90b34e889c52c9721f7680087e452f14a8d765b
CodinMaster <latheyapoorv@gmail.com>
2019-11-15 20:21:11 +0530

    addCandidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,27 @@ pragma solidity 0.5.8;
 
 contract Election {
 
-  string public candidate;
+  //model a candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  //store candidates
+  //key is id here
+  mapping(uint => Candidate) public candidates; //fetch candidates. public: auto getter function
+
+  //store candidates count
+  uint public candidatesCount;
 
   constructor () public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate (string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
